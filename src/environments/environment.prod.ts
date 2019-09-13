25b58230ce6627fe3e7a453b052506a1b5df9c72
vito madio <vitomadio@hotmail.com>
2019-09-13 16:45:46 +0200

    Change url for images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
 
   /* Google Maps API Key */
   apiKey: 'AIzaSyCofVUjA3wviN9YpTnA8i4QJ6BZ1qdXIG4',
-  url: 'http://ec2-35-180-61-152.eu-west-3.compute.amazonaws.com:8000'
+  url: 'https://garagesale-app.herokuapp.com'
 };
