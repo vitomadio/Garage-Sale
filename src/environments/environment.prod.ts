7f6a61128c55f5ab268e6b8249ef4c0a932ca9b0
vito madio <vitomadio@hotmail.com>
2019-09-13 16:23:50 +0200

    another commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+
+export const environment = {
+  production: true,
+
+  /* Google Maps API Key */
+  apiKey: 'AIzaSyCofVUjA3wviN9YpTnA8i4QJ6BZ1qdXIG4',
+  url: 'http://ec2-35-180-61-152.eu-west-3.compute.amazonaws.com:8000'
+};
